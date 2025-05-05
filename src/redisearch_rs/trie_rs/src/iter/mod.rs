cba8477740404daae1aaf1f316989150f386fb01
LukeMathWalker <20745048+LukeMathWalker@users.noreply.github.com>
2025-05-05 15:14:22 +0200

    WIP specialised iter

--- src/redisearch_rs/trie_rs/src/iter/mod.rs
+++ src/redisearch_rs/trie_rs/src/iter/mod.rs
@@ -1,9 +1,11 @@
 //! Different iterators to traverse a [`TrieMap`](crate::TrieMap).
 pub mod filter;
+mod into_values;
 mod iter_;
 mod lending;
 mod values;
 
+pub use into_values::IntoValues;
 pub use iter_::Iter;
 pub use lending::LendingIter;
 pub use values::Values;
